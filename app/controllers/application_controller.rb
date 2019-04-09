5b311e9fbdc3f0e013344242583e969f58da002e
Pritchard45 <alexpritchard45@gmail.com>
2019-04-08 20:27:45 -0400

    sign-up-in-out working / disabled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  #protect_from_forgery with: :exception
+
+  #before_action :authenticate_user!
+
+  # commenting for default creating
+  # there does not need to be a user yet but functionality works
 end
